9180a8c83027f2afd1bd1b2c30558a0fbf86d352
Zheeeng <hi@zheeeng.me>
2023-09-18 19:08:12 +0800

    init

---
+++ README.md
@@ -0,0 +1,80 @@
+# rehype-meta-string
+
+<div align="center">
+
+[![Known Vulnerabilities][known-vulnerabilities-image]][known-vulnerabilities-url]
+[![Maintainability][maintainability-image]][maintainability-url]
+![publish workflow][publish-workflow-image]
+[![license][license-image]][license-url]
+[![GitHub issues][github-issues-image]][github-issues-url]
+![NPM bundle size(minified + gzip)][bundle-size-image]
+
+[known-vulnerabilities-image]: https://snyk.io/test/github/zheeeng/rehype-meta-string/badge.svg
+[known-vulnerabilities-url]: https://snyk.io/test/github/zheeeng/rehype-meta-string
+
+[maintainability-image]: https://api.codeclimate.com/v1/badges/d3eaf22221bf57742429/maintainability
+[maintainability-url]: https://codeclimate.com/github/zheeeng/rehype-meta-string/maintainability
+
+[publish-workflow-image]: https://github.com/zheeeng/rehype-meta-string/actions/workflows/publish.yml/badge.svg
+
+[license-image]: https://img.shields.io/github/license/mashape/apistatus.svg
+[license-url]: https://github.com/zheeeng/rehype-meta-string/blob/master/LICENSE
+
+[github-issues-image]: https://img.shields.io/github/issues/zheeeng/rehype-meta-string
+[github-issues-url]: https://github.com/zheeeng/rehype-meta-string/issues
+
+[bundle-size-image]: https://img.shields.io/bundlephobia/minzip/rehype-meta-string.svg
+
+[![NPM](https://nodei.co/npm/rehype-meta-string.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/rehype-meta-string/)
+
+</div>
+
+> A rehype plugin for parsing the meta string of markdown code fence.
+
+This plugin parse the meta string into key-value paris and assign them to `code` tag ast nodes.
+
+The parsing contains rules below:
+
+* **`true`**: empty or literal value `true` is treated as `true`
+* **`false`**: literal value `false` is treated as `false`
+* **word**: literal string without spaces is treated as word.
+* **sentence**: double quoted, singled quoted, back-ticked string are treated as string.
+* **range**: square brackets wrapped string is treated as range numbers. It requires format like `{digits}-{digits}`.
+* **mixed**: mix all the markers above.
+
+There is the illustration table:
+
+| meta string     | parsed |
+| --------------- | ------------------- |
+| foo             | { foo: true }       |
+| foo=true        | { foo: false }      |
+|
+| foo=false       | { foo: false }      |
+|
+| foo=bar         | { foo: "bar" }      |
+|
+| foo="false"     | { foo: "false" }    |
+| foo='false'     | { foo: "false" }    |
+| foo=\`false\`   | { foo: "false" }    |
+| foo="true"      | { foo: "true" }     |
+| foo='true'      | { foo: "true" }     |
+| foo=\`true\`    | { foo: "true" }     |
+| foo="bar baz"   | { foo: "bar baz" }  |
+| foo='bar baz'   | { foo: "bar baz" }  |
+| foo=\`bar baz\` | { foo: "bar baz" }  |
+|
+| foo=[42-71]     | { foo: [42, 71] }   |
+|
+| foo foo1=true foo2=false foo3=bar foo4="true" foo5='false' foo6=\`hello world\` foo7=[42-71] | { foo: true, foo1: true, foo2: false, foo3: "bar", foo4: "true", foo5: "false", foo6="hello world", foo7: [42, 71] }
+
+## 🧩  Installation
+
+```bash
+yarn add rehype-meta-string (or npm/pnpm)
+```
+
+## 💫 Options
+
+* **metaString**?: string
+
+> Specify the field name which holds the raw meta string. By default, it is `metaString`.
---
+++ index.ts
@@ -0,0 +1,211 @@
+import type { Root } from 'hast'
+import { visit } from 'unist-util-visit'
+
+const lexer = (input: string): string[] => {
+  const [lexed, num] = input.split('').reduce<[lexed: string[], num: string]>(
+    ([lexed, num], char) => {
+      switch (char) {
+        case ' ': {
+          if (num) {
+            lexed.push(num)
+          }
+
+          return [lexed, '']
+        }
+        case '=':
+        case '"':
+        case '\'':
+        case '`':
+        case '[':
+        case ']': {
+          if (num) {
+            lexed.push(num)
+          }
+
+          lexed.push(char)
+
+          return [lexed, '']
+        }
+        default: {
+          const newNum = num + char
+
+          return [lexed, newNum]
+        }
+      }
+    },
+    [[], '']
+  )
+
+  if (num) {
+    lexed.push(num)
+  }
+
+  return lexed
+}
+
+const parser = (input: string): Record<string, string | boolean | [start: number, end: number]> => {
+  const lexed = lexer(input)
+
+  function S () {
+    return Expression()
+  }
+
+  function Key (): string {
+    const lexeme = lexed.shift()
+
+    if (lexeme === undefined) {
+      throw 'invalid key expr'
+    }
+
+    switch (lexeme) {
+      case '=':
+      case '"':
+      case '\'':
+      case '`':
+      case '[':
+      case ']': {
+        throw 'invalid key expr'
+      }
+    }
+
+    return lexeme
+  }
+
+  function Sentence (endToken: string): string {
+    let sentence = ''
+
+
+    while (lexed[0] !== endToken) {
+      sentence += lexed.shift()
+    }
+
+    return sentence
+  }
+
+
+  function Range (): [start: number, end: number] {
+
+    const lexeme = lexed.shift()
+
+    if (lexeme === undefined) {
+      throw 'invalid range expr'
+    }
+
+    const matched = lexeme.match(/^(\d+)-(\d+)$/)
+
+    if (!matched) {
+      throw 'invalid range expr'
+    }
+
+    const [_, start, end] = matched
+
+    if (isNaN(+start) || isNaN(+end)) {
+      throw 'invalid range expr'
+    }
+
+    return [+start, +end]
+  }
+
+  function Value (): string | boolean | [start: number, end: number] {
+    const lexeme = lexed.shift()
+
+    if (lexeme === undefined) {
+      throw 'invalid value expr'
+    }
+    if (lexeme === '=') {
+      const nextLexeme = lexed.shift()
+
+      if (nextLexeme === undefined) {
+        throw 'invalid value expr'
+      }
+
+      if (nextLexeme === 'false') {
+        return false
+      } else if (nextLexeme === 'true') {
+        return true
+      }
+
+      if (nextLexeme === ']') {
+        throw 'invalid value expr'
+      } else if (nextLexeme === '[') {
+        const value = Range()
+
+        if (lexed.shift() !== ']') {
+          throw 'invalid value expr'
+        }
+
+        return value
+      } else if (nextLexeme === '"' || nextLexeme === '\'' || nextLexeme === '`') {
+        const sentence = Sentence(nextLexeme)
+
+        if (lexed.shift() !== nextLexeme) {
+          throw 'invalid value expr'
+        }
+
+        return sentence
+      }
+
+      return nextLexeme
+    } else {
+      return true
+    }
+  }
+
+  function Expression (): Record<string, string | boolean | [start: number, end: number]> {
+    if (!lexed.length) {
+      return {}
+    }
+
+    const [key, value] = [Key(), Value()]
+
+    return {
+      [key]: value,
+      ...Expression(),
+    }
+  }
+
+  const result = S()
+
+  if (lexed.length) {
+    throw 'invalid expr'
+  }
+
+  return result
+}
+
+const parseMeta = (input: string) => {
+  const parsed = parser(input)
+
+  return {
+    ...parsed,
+    metaString: input,
+  }
+}
+
+export type RehypeMetaType<T extends Record<string, string | boolean | [start: number, end: number]>, S = 'metaString'> = T & Record<S & string, string>
+
+export type RehypeMetaStringOptions = {
+  /**
+   * Specify the field name which holds the raw meta string.
+   * By default, it is `metaString`. 
+   */
+  metaString?: string,
+}
+
+export const rehypeMetaString = (options: RehypeMetaStringOptions) => {
+  return (tree: Root) => {
+    visit(tree, 'element', (node) => {
+      if (node.tagName === 'code' && node.data && 'meta' in node.data && node.data.meta && typeof node.data.meta === 'string') {
+        const meta = node.data.meta
+
+        const parsed = parser(meta)
+
+        Object.assign(node.properties, parsed)
+
+        if (options.metaString) {
+          node.properties[options.metaString] = meta
+        }
+      }
+    })
+  }
+}
